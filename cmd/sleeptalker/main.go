164af412eac375b80d992fcf8efc4ffbc00d22ec
Julz Friedman <julz.friedman@uk.ibm.com>
2020-08-24 09:54:23 +0100

    Add comments to example app

--- cmd/sleeptalker/main.go
+++ cmd/sleeptalker/main.go
@@ -8,11 +8,15 @@ import (
 
 func main() {
 	go func() {
+		// prints all the time in the background, even when no requests are being
+		// processed - naughty!.
 		for range time.Tick(500 * time.Millisecond) {
 			fmt.Printf("Ticking at %s.\n", time.Now().Format("3h 04m 05.000s"))
 		}
 	}()
 
+	// simple handler, just sleeps 2 seconds - backgrond tasks allowed to run
+	// while this is going.
 	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello, this request will take two seconds..")
 		w.(http.Flusher).Flush()
